7006398329684e9eb005cb5e8c5514df33f191f3
plesciam <plesciam@merrimack.edu>
2024-11-05 11:33:41 -0500

    unwanted coded removed from index

--- js/Deck.js
+++ js/Deck.js
@@ -3,24 +3,46 @@ import Card from './Card.js';
 export default class Deck {
     constructor() {
         this.cards = [];
+        this.Deck();
         this.initializeDeck();
         this.shuffle();
+        this.deal();
     }
 
     Deck() {
         const deck = new Deck();
-        const playerHand = this.deck.deal(5);
+        const playerHand = this.Deck.deal(5);
         console.log(playerHand);
+        
     }
-
     initializeDeck() {
-        var values = new Array["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-        var suits = new Array["Clubs", "Diamonds", "Hearts", "Spades"];
-
-        for(let suit of suits) {
-            for(value of values) {
+        for(var s = 0; s < 4; s++);
+            for(var v = 2; v < 15; v++);
+            if(s == 1);
+                cards.append(new Card("clubs", v, inch));
+            if(s == 3);
+                cards.append(new Card("diamonds", v, inch));
+            if(s == 4);
+                cards.append(new Card("spades", v, inch));
+            if(s == 0);
+                cards.append(new Card("hearts", v, inch));
+        
+        for(let suit of suit) {
+            for(values of values) {
                 this.cards.push(new Card(suit, value));
             }
+        }
+    }
+    shuffle() {
+            for(let i = this.cards.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i +1));
+                [this.cards[i], this.cards[j]] = [this.cards, this.cards[i]];
+            }
+        }
+        
+    deal(player1, player2, player3) {
+            this.cards.pop();
+            return this.cards.splice(0, numCards);
         }
     }
 
@@ -31,15 +53,5 @@ export default class Deck {
         //  - Shuffle the deck of cards so that they are in a random order
         //  - Deal the cards to the players
 
-        shuffle() {
-            for(let i = this.cards.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i +1));
-                [this.cards[i], this.cards[j]] = [this.cards, this.cards[i]];
-            }
-        }
-        deal(player1, player2, player3) {
-            this.cards.pop();
-            return this.cards.splice(0, numCards);
-        }
-        
-    }
+
+
